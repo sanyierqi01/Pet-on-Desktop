98f1d0aee5c97f2d0ce1588cb49ff4e5e2af7a80
sanyierqi <blackalandai@gmail.com>
2026-09-19 23:37:05 +0800

    Delete pet directory

--- pet/start-pet.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-pwsh.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -STA -File "%~dp0desktop-pet.ps1"
-if errorlevel 1 (
-    echo.
-    echo The desktop pet exited with an error.
-    pause
-)
